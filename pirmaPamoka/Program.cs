5a23e9e3fd2ca5792ee5bf150e568ee32f10bfa6
Tadas <tadas1985@gmail.com>
2021-12-20 21:18:16 +0200

    Created new application

---
+++ pirmaPamoka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pirmaPamoka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ooi");
+            Console.ReadKey();
+        }
+    }
+}
