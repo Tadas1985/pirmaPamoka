c26963f5da94ab205eec13e70cbeab6273a084bc
Tadas <tadas1985@gmail.com>
2021-12-20 21:50:55 +0200

    Mokinames su git extentions

--- pirmaPamoka/Program.cs
+++ pirmaPamoka/Program.cs
@@ -10,7 +10,7 @@ namespace pirmaPamoka
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ooi1121");
+            Console.WriteLine("ooiffa1121");
             Console.ReadKey();
         }
     }
